7c9e2d6fa1bdf0b73fd38f415dc8e52a2c6611ff
System Administrator <root@Apples-Mac-mini.local>
2018-09-14 18:22:50 +0530

    amir

--- routes/api/index.js
+++ routes/api/index.js
@@ -1322,6 +1322,66 @@ router.post('/upload-profile-video', passport.authenticate('jwt', { session : fa
 
   
 })
+router.post('/video-set-private', passport.authenticate('jwt', { session : false }), (req, res) => {
+
+  User.findById(req.user.id).then(user => {
+    
+ for(let i =0;i<user.videos.length;i++){
+  if(user.videos[i]._id == req.body.id){
+    user.videos[i].access = 'private';
+  }
+ }
+ if(user.save()){
+  return res.json({
+    success: true,
+    code: 200,
+    info: user
+  });
+ }
+  })
+  
+})
+router.post('/video-delete', passport.authenticate('jwt', { session : false }), (req, res) => {
+
+  User.findById(req.user.id).then(user => {
+    
+ for(let i =0;i<user.videos.length;i++){
+  if(user.videos[i]._id == req.body.id){
+    //console.log(user.videos[i].access);
+    user.videos.splice(i, 1);
+  }
+ }
+ if(user.save()){
+  return res.json({
+    success: true,
+    code: 200,
+    info: user
+  });
+ }
+  })
+  
+})
+router.post('/video-update', passport.authenticate('jwt', { session : false }), (req, res) => {
+
+  User.findById(req.user.id).then(user => {
+    
+ for(let i =0;i<user.videos.length;i++){
+  if(user.videos[i].url == req.body.url){
+    //console.log(user.videos[i].access);
+    user.videos[i].access = req.body.access;
+    user.videos[i].altTag = req.body.tag;
+  }
+ }
+ if(user.save()){
+  return res.json({
+    success: true,
+    code: 200,
+    info: user
+  });
+ }
+  })
+  
+})
 
 
 function diff_years(dt2, dt1) 
