a0dd64f56d9487e572ced37e6256ba486214dd29
System Administrator <root@Apples-Mac-mini.local>
2018-09-20 18:28:52 +0530

    amir

--- routes/api/index.js
+++ routes/api/index.js
@@ -518,12 +518,127 @@ router.post('/change-password',passport.authenticate('jwt', {session : false}), 
                 }
               }).then(function (result) {
                 if(result) {
+                  var email_body = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+                  <html xmlns="http://www.w3.org/1999/xhtml">
+                  
+                  <head>
+                  
+                      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                  
+                      <title>Password Updated</title>
+                  
+                      <style>
+                  
+                          body {
+                  
+                              background-color: #FFFFFF; padding: 0; margin: 0;
+                  
+                          }
+                  
+                      </style>
+                  
+                  </head>
+                  
+                  <body style="background-color: #FFFFFF; padding: 0; margin: 0;">
+                  
+                  <table border="0" cellpadding="0" cellspacing="10" height="100%" bgcolor="#FFFFFF" width="100%" style="max-width: 650px;" id="bodyTable">
+                  
+                      <tr>
+                  
+                          <td align="center" valign="top">
+                  
+                              <table border="0" cellpadding="0" cellspacing="0" width="100%" id="emailContainer" style="font-family:Arial; color: #333333;">
+                  
+                                  <!-- Logo -->
+                  
+                                  <tr>
+                  
+                                      <td align="left" valign="top" colspan="2" style="border-bottom: 1px solid #CCCCCC; padding-bottom: 10px;">
+                  
+                                          
+                  
+                                      </td>
+                  
+                                  </tr>
+                  
+                                  <!-- Title -->
+                  
+                                  <tr>
+                  
+                                      <td align="left" valign="top" colspan="2" style="border-bottom: 1px solid #CCCCCC; padding: 20px 0 10px 0;">
+                  
+                                          <span style="font-size: 18px; font-weight: normal;">Password Changed Status</span>
+                  
+                                      </td>
+                  
+                                  </tr>
+                  
+                                  <!-- Messages -->
+                  
+                                  <tr>
+                  
+                                      <td align="left" valign="top" colspan="2" style="padding-top: 10px;">
+                  
+                                          <span style="font-size: 12px; line-height: 1.5; color: #333333;">
+                  
+                                            
+                                                    Your password successful updated.
+                  
+                                    
+                  
+              
+                                              <br/>
+              
+                                              
+                  
+                                              <br/><br/>
+                  
+                                              We recommend that you keep your password secure and not share it with anyone.If you feel your password has been compromised, you can change it by going to your Change password page and clicking on the "Change Password" link.
+                  
+                                              <br/><br/>
+                  
+                                              If you need help, or you have any other questions, feel free to email info@wrctpl.com, or call customer service toll-free at +91-1234567890.
+                  
+                                              <br/><br/>
+                  
+                                             Kinky - AN online dating service
+                  
+                                          </span>
+                  
+                                      </td>
+                  
+                                  </tr>
+                  
+                              </table>
+                  
+                          </td>
+                  
+                      </tr>
+                  
+                  </table>
+                  
+                  </body>
+                  
+                  </html> `;
+              
+                  var sendEmail = Mailjet.post('send');
+               
+                  var emailData = {
+                      'FromEmail': 'info@wrctpl.com',
+                      'FromName': 'Kinky - An online dating service',
+                      'Subject': 'Password Updated',
+                      'Html-part': email_body,
+                      'Recipients': [{'Email': req.user.email}]
+                  }; 
+                  if(sendEmail.request(emailData)) {
                   return res.json({
                     success: true,
                     code:200,
                     message: "Password changed successfully."
                   });
                 }
+              }
               });
           });
         });
