9bea2b566e531f111bacff463c0bcc3a5c9da494
Manomit Mitra <manomit@wrctechnologies.com>
2018-11-08 14:01:53 +0530

    commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -48,8 +48,8 @@ const pusher = new Pusher({
 });
 
 var s3 = new AWS.S3({
-  accessKeyId: process.env.accessKeyId,
-  secretAccessKey: process.env.secretAccessKey
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
 var upload = multer({
